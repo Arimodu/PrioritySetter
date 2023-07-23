17e7d143e4ad09ab436ceb7cdef393432c0494f3
Arimodu <44728973+Arimodu@users.noreply.github.com>
2023-07-23 23:50:35 +0200

    Add periodic checking of priority, 1.1.0 release

--- PrioritySetter/Plugin.cs
+++ PrioritySetter/Plugin.cs
@@ -2,6 +2,7 @@
 using IPA.Config;
 using IPA.Config.Stores;
 using System.Diagnostics;
+using System.Threading;
 using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 
@@ -14,6 +15,7 @@ namespace PrioritySetter
         private readonly Process ThisProcess;
         private readonly IPALogger Logger;
         private readonly PrioritySetterConfig Config;
+        private readonly Timer RefreshTimer;
 
         [Init]
         public Plugin(IPALogger logger, Config config)
@@ -25,12 +27,60 @@ namespace PrioritySetter
             SetPriority();
 
             Config.OnChanged += Config_OnChanged;
+
+            // Start with 10 seconds left, then 120 seconds (or two minutes) between checks
+            RefreshTimer = new Timer(RefreshPriority, null, 10000, 120000);
+        }
+
+        [OnExit] 
+        public void OnExit()
+        {
+            RefreshTimer.Dispose();
         }
 
         private void Config_OnChanged()
         {
             Logger.Info("Config changed! Updating process priority.");
             SetPriority();
+        }
+
+        private void RefreshPriority(object state)
+        {
+            ProcessPriorityClass ppc;
+
+            switch (Config.ProcessPriority)
+            {
+                case Priority.Idle:
+                    ppc = ProcessPriorityClass.Idle;
+                    break;
+                case Priority.BelowNormal:
+                    ppc = ProcessPriorityClass.BelowNormal;
+                    break;
+                case Priority.Normal:
+                    ppc = ProcessPriorityClass.Normal;
+                    break;
+                case Priority.AboveNormal:
+                    ppc = ProcessPriorityClass.AboveNormal;
+                    break;
+                case Priority.High:
+                    ppc = ProcessPriorityClass.High;
+                    break;
+                case Priority.RealTime:
+                    ppc = ProcessPriorityClass.RealTime;
+                    break;
+                default:
+                    ppc = ProcessPriorityClass.Normal;
+                    break;
+            }
+
+            // Debug log here
+            //Logger.Notice($"Cheking priority. Set: {ThisProcess.PriorityClass}, Desired: {Config.ProcessPriority}, Desired converted: {ppc}");
+
+            if (ThisProcess.PriorityClass == ppc) return;
+
+            Logger.Info($"Priority no longer at desired level. Refreshing!");
+
+            Logger.Info($"Set priority to {ThisProcess.PriorityClass}");
         }
 
         internal void SetPriority()
@@ -63,7 +113,7 @@ namespace PrioritySetter
             }
 
             ThisProcess.PriorityClass = ppc;
-            Logger.Info($"Set priority to {ppc}");
+            Logger.Info($"Set priority to {ThisProcess.PriorityClass}");
         }
     }
 }
